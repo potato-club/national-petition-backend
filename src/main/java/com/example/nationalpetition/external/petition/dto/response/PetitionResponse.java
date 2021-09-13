27114362c48844c15ecbd77496030c5d0437e44a
YuSunjo <tnswh2023@gmail.com>
2021-09-13 14:38:00 +0900

    feat(board): id로 크롤링서버 콜해서 생성

--- src/main/java/com/example/nationalpetition/external/petition/dto/response/PetitionResponse.java
+++ src/main/java/com/example/nationalpetition/external/petition/dto/response/PetitionResponse.java
@@ -15,4 +15,15 @@ public class PetitionResponse {
 
 	private String status;
 
+	public PetitionResponse(String title, String content, String petitionsCount, String status) {
+		this.title = title;
+		this.content = content;
+		this.petitionsCount = petitionsCount;
+		this.status = status;
+	}
+
+	public static PetitionResponse of(String title, String content, String petitionsCount, String status) {
+		return new PetitionResponse(title, content, petitionsCount, status);
+	}
+
 }
