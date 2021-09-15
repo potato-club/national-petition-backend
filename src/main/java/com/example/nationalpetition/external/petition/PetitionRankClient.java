30803470e54ae9cdfdc258169edd8a82d9fdba36
seungh0 <will.seungho@gmail.com>
2021-09-15 17:55:25 +0900

    feat: 국민 청원 인기 TOP5 API Client

---
+++ src/main/java/com/example/nationalpetition/external/petition/PetitionRankClient.java
@@ -0,0 +1,9 @@
+package com.example.nationalpetition.external.petition;
+
+import com.example.nationalpetition.external.petition.dto.response.PetitionRankResponse;
+
+public interface PetitionRankClient {
+
+	PetitionRankResponse getRank();
+
+}
