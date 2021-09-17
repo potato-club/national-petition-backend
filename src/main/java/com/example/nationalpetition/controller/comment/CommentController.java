b55ba369381ae2c15030194302c9b1091c611b28
yerimkoko <gochi97@naver.com>
2021-09-17 13:04:05 +0900

    feat: comment 생성 (일단 생성만, 대댓글은 추후 적용)

--- src/main/java/com/example/nationalpetition/controller/comment/CommentController.java
+++ src/main/java/com/example/nationalpetition/controller/comment/CommentController.java
@@ -4,20 +4,18 @@ import com.example.nationalpetition.controller.ApiResponse;
 import com.example.nationalpetition.dto.CommentCreateDto;
 import com.example.nationalpetition.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class CommentController {
 
-    private final CommentService service;
+    private final CommentService commentService;
 
     @PostMapping("/api/v1/comment/{boardId}")
-    public ApiResponse<Long> save(@RequestBody CommentCreateDto dto, @PathVariable Long boardId){
-        return ApiResponse.success(service.save(dto, boardId));
+    public ApiResponse<Long> addComment(@RequestBody CommentCreateDto dto,
+                                        @PathVariable Long boardId) {
+        return ApiResponse.success(commentService.addComment(dto, boardId));
     }
 
 }
