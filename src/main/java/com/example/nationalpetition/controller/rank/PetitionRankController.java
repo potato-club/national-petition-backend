022cfca4efe9779c75c4e5cc5e40ea7b3ffa0e92
seungh0 <will.seungho@gmail.com>
2021-09-24 23:19:59 +0900

    아 몰라 일단 스웨거 적용함

--- src/main/java/com/example/nationalpetition/controller/rank/PetitionRankController.java
+++ src/main/java/com/example/nationalpetition/controller/rank/PetitionRankController.java
@@ -3,6 +3,7 @@ package com.example.nationalpetition.controller.rank;
 import com.example.nationalpetition.controller.ApiResponse;
 import com.example.nationalpetition.external.petition.PetitionRankClient;
 import com.example.nationalpetition.external.petition.dto.response.PetitionRankResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class PetitionRankController {
 
 	private final PetitionRankClient petitionRankClient;
 
+	@Operation(summary = "실시간으로 국민청원 인기 TOP5을 조회하는 API")
 	@GetMapping("/api/v1/petition/rank")
 	public ApiResponse<PetitionRankResponse> getPetitionRank() {
 		return ApiResponse.success(petitionRankClient.getRank());
