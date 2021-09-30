a79b5a2068393e670bca6891a5cb4e12f215366f
hyun6ik <fkanwm@naver.com>
2021-09-30 15:26:46 +0900

    [BAC-30] 내가 작성한 글 리스트 좋아요 싫어요 개수 추가 및 테스트 코드 개발 - V5

--- src/test/java/com/example/nationalpetition/service/member/MemberServiceTest.java
+++ src/test/java/com/example/nationalpetition/service/member/MemberServiceTest.java
@@ -61,6 +61,10 @@ public class MemberServiceTest {
     @AfterEach
     public void clear() {
         memberRepository.deleteAll();
+        boardRepository.deleteAll();
+        boardLikeRepository.deleteAll();
+        commentRepository.deleteAll();
+        deleteMemberRepository.deleteAll();
     }
 
     @Test
