d1484973db489a88eeeaf717f629c5d366df5ef5
Seungho Kang <will.seungho@gmail.com>
2021-10-03 15:56:27 +0900

    feat: Cors 설정 (yerim) (#59)
    * feat: cors 적용
    
    * fix: 기존 Cors 설정 제거
    
    Co-authored-by: yerimkoko <gochi97@naver.com>

--- src/main/java/com/example/nationalpetition/config/WebConfig.java
+++ src/main/java/com/example/nationalpetition/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.nationalpetition.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,6 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(memberIdArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*");
 	}
 
 }
