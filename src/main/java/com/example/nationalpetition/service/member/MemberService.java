4e0104ddb32010319253c8e7f7133d9100a4d8d8
hyun6ik <fkanwm@naver.com>
2021-10-07 18:30:48 +0900

    fix(addNickName) : 리턴 타입 변경

--- src/main/java/com/example/nationalpetition/service/member/MemberService.java
+++ src/main/java/com/example/nationalpetition/service/member/MemberService.java
@@ -4,7 +4,6 @@ import com.example.nationalpetition.domain.member.entity.Member;
 import com.example.nationalpetition.dto.board.response.BoardInfoResponseInMyPage;
 import com.example.nationalpetition.dto.member.request.NickNameRequest;
 import com.example.nationalpetition.dto.member.response.MemberResponse;
-import com.example.nationalpetition.utils.message.MessageType;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -14,9 +13,9 @@ public interface MemberService {
 
     Member findByEmail(String email);
 
-    MessageType addNickName(Long memberId, NickNameRequest request);
+    String addNickName(Long memberId, NickNameRequest request);
 
-    MessageType deleteMember(Long memberId);
+    String deleteMember(Long memberId);
 
     List<BoardInfoResponseInMyPage> getMyBoardList(Long memberId, Pageable pageable);
 }
