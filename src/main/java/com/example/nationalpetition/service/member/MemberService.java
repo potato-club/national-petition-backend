54f5846803245b938273c9f033314581d16bd08b
hyun6ik <fkanwm@naver.com>
2021-10-14 16:56:08 +0900

    fix(mypagePageNation) : 마이페이지 게시글 가져오기 PageNation 수정

--- src/main/java/com/example/nationalpetition/service/member/MemberService.java
+++ src/main/java/com/example/nationalpetition/service/member/MemberService.java
@@ -1,12 +1,10 @@
 package com.example.nationalpetition.service.member;
 
 import com.example.nationalpetition.domain.member.entity.Member;
-import com.example.nationalpetition.dto.board.response.BoardInfoResponseInMyPage;
+import com.example.nationalpetition.dto.member.request.MemberPageRequest;
 import com.example.nationalpetition.dto.member.request.NickNameRequest;
 import com.example.nationalpetition.dto.member.response.MemberResponse;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
+import com.example.nationalpetition.dto.member.response.MyPageBoardListResponse;
 
 public interface MemberService {
     MemberResponse findById(Long memberId);
@@ -17,5 +15,5 @@ public interface MemberService {
 
     String deleteMember(Long memberId);
 
-    List<BoardInfoResponseInMyPage> getMyBoardList(Long memberId, Pageable pageable);
+    MyPageBoardListResponse getMyBoardList(Long memberId, MemberPageRequest request);
 }
