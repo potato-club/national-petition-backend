8db42fc45492bbff1aeb9e2e544ededfd0893425
will <>
2021-10-29 13:19:34 +0900

    fix: CI/CD 테스트용

--- src/main/java/com/example/nationalpetition/controller/MainController.java
+++ src/main/java/com/example/nationalpetition/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-	@GetMapping({"/", "/ping"})
+	@GetMapping("/")
 	public ApiResponse<String> ping() {
 		return ApiResponse.OK;
 	}
